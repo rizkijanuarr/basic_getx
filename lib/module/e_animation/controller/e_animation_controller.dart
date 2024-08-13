7939eff5d39fa466630d49d7a57ab871572543b6
rizkijanuarr <72480809+rizkijanuarr@users.noreply.github.com>
2024-08-13 08:21:37 +0700

    E. Animation (Done)

--- lib/module/e_animation/controller/e_animation_controller.dart
+++ lib/module/e_animation/controller/e_animation_controller.dart
@@ -1,9 +1,68 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import '../view/e_animation_view.dart';
+import 'package:basic_getx/core.dart';
 
 class EAnimationController extends GetxController {
   EAnimationView? view;
 
+  // #1
+  bool isAnimated = false;
 
+  // #2
+  updateAnimatedState() {
+    isAnimated = !isAnimated;
+    update();
+  }
 
+  // START - TAMPILKAN KODE!
+  final String data = '''
+ // #1
+ bool isAnimated = false;
+
+ // #2
+  updateAnimatedState() { // FoldStart: section1
+    isAnimated = !isAnimated;
+    update();
+  } // FoldEnd: section1
+ ''';
+
+  final String vieew = '''
+// #3
+AnimatedContainer( // FoldStart: section1
+  duration: const Duration(milliseconds: 1000),
+  height: 100.0,
+  width: controller.isAnimated ? 300 : 100,
+  decoration: BoxDecoration(
+    color: controller.isAnimated ? Colors.red : Colors.blue,
+    borderRadius: const BorderRadius.all(
+      Radius.circular(8.0),
+    ),
+  ),
+), // FoldEnd: section1
+''';
+
+  late final CodeController dataController;
+  late final CodeController vieewController;
+
+  EAnimationController() {
+    dataController = CodeController(
+      text: data,
+      language: dart,
+      namedSectionParser: const BracketsStartEndNamedSectionParser(),
+    );
+
+    vieewController = CodeController(
+      text: vieew,
+      language: dart,
+      namedSectionParser: const BracketsStartEndNamedSectionParser(),
+    );
+
+    // Fold otomatis
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      dataController.foldOutsideSections(['section1']);
+      vieewController.foldOutsideSections(['section1']);
+    });
+  }
+
+  // END - TAMPILKAN KODE!
 }
--- lib/module/e_animation/view/e_animation_view.dart
+++ lib/module/e_animation/view/e_animation_view.dart
@@ -20,10 +20,260 @@ class EAnimationView extends StatelessWidget {
           body: SingleChildScrollView(
             child: Container(
               padding: const EdgeInsets.all(10.0),
-              child: const Column(
-                children: [],
+              child: Column(
+                children: [
+                  Column(
+                    children: [
+                      const Text(
+                        "AnimatedContainer() - width - color",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      AnimatedContainer(
+                        duration: const Duration(milliseconds: 1000),
+                        height: 100.0,
+                        width: controller.isAnimated ? 300 : 100,
+                        decoration: BoxDecoration(
+                          color:
+                              controller.isAnimated ? Colors.red : Colors.blue,
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(
+                              8.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Divider(),
+                  Column(
+                    children: [
+                      const Text(
+                        "AnimatedContainer() - height - color",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      AnimatedContainer(
+                        duration: const Duration(milliseconds: 1000),
+                        width: 100.0,
+                        height: controller.isAnimated ? 150 : 100,
+                        decoration: BoxDecoration(
+                          color: controller.isAnimated
+                              ? Colors.indigoAccent
+                              : Colors.brown,
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(
+                              8.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Divider(),
+                  Column(
+                    children: [
+                      const Text(
+                        "AnimatedContainer() - margin - color grad",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      AnimatedContainer(
+                        duration: const Duration(milliseconds: 1000),
+                        height: 100.0,
+                        width: 100.0,
+                        margin: EdgeInsets.only(
+                          top: controller.isAnimated ? 50 : 0,
+                          right: controller.isAnimated ? 50 : 0,
+                        ),
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: controller.isAnimated
+                                ? [Colors.red, Colors.grey[300]!]
+                                : [Colors.grey[300]!, Colors.red],
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                          ),
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(
+                              8.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Divider(),
+                  Column(
+                    children: [
+                      const Text(
+                        "AnimatedRotation() - turns - 180°",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      AnimatedRotation(
+                        turns: (controller.isAnimated ? 180 : 0),
+                        duration: const Duration(milliseconds: 900),
+                        child: const CircleAvatar(
+                          radius: 40.0,
+                          backgroundImage: AssetImage(
+                            "assets/img/unp.jpg",
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Divider(),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "AnimatedContainer() - margin left 80",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      AnimatedContainer(
+                        duration: const Duration(milliseconds: 900),
+                        height: 70.0,
+                        width: 70.0,
+                        margin: EdgeInsets.only(
+                          left: controller.isAnimated ? 80 : 0,
+                        ),
+                        child: const CircleAvatar(
+                          radius: 40.0,
+                          backgroundImage: AssetImage(
+                            "assets/img/ancit.png",
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Divider(),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const Text(
+                        "AnimatedContainer() - margin right 80",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      AnimatedContainer(
+                        duration: const Duration(milliseconds: 900),
+                        height: 70.0,
+                        width: 70.0,
+                        margin: EdgeInsets.only(
+                          right: controller.isAnimated ? 80 : 0,
+                        ),
+                        child: const CircleAvatar(
+                          radius: 40.0,
+                          backgroundImage: AssetImage(
+                            "assets/img/kikew.jpg",
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Divider(),
+                  Column(
+                    children: [
+                      const Text(
+                        "AnimatedScale() - scale zoom 5 : 1",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      AnimatedScale(
+                        scale: controller.isAnimated ? 5.0 : 1.0,
+                        duration: const Duration(milliseconds: 1800),
+                        child: const CircleAvatar(
+                          radius: 20.0,
+                          backgroundImage: AssetImage(
+                            "assets/img/ancit.png",
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Divider(),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Controller :",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(12.0),
+                        child: CodeTheme(
+                          data: CodeThemeData(styles: xt256Theme),
+                          child: SingleChildScrollView(
+                            child: CodeField(
+                              controller: controller.dataController,
+                              gutterStyle: const GutterStyle(
+                                showErrors: false,
+                                showFoldingHandles: true,
+                                showLineNumbers: false,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      //
+                      const SizedBox(
+                        height: 10.0,
+                      ),
+                      const Text(
+                        "View :",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(12.0),
+                        child: CodeTheme(
+                          data: CodeThemeData(styles: xt256Theme),
+                          child: SingleChildScrollView(
+                            child: CodeField(
+                              controller: controller.vieewController,
+                              gutterStyle: const GutterStyle(
+                                showErrors: false,
+                                showFoldingHandles: true,
+                                showLineNumbers: false,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
+          ),
+          // Floating Button
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: controller.isAnimated ? Colors.green : Colors.red,
+            foregroundColor:
+                controller.isAnimated ? Colors.white : Colors.black,
+            child: Icon(controller.isAnimated ? Icons.check : Icons.cancel),
+            onPressed: () => controller.updateAnimatedState(),
           ),
         );
       },
